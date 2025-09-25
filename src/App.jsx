db757c293c223649bbc920da42300441de0c488b
Noah Milhac <noahmilhac@MacBook-Air-de-Noah.local>
2025-09-25 23:15:52 +0200

    Initial commit - site vitrine

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-red-200 flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Tailwind est bien actif ✅</h1>
+    </div>
+  )
+}
